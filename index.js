ca700c13013c2a0266e10e8d433b73c7daf49e84
Jiyarathore <jiyar3472@gmail.com>
2022-08-26 23:27:44 +0530

    json

--- index.js
+++ index.js
@@ -3,7 +3,27 @@
 
 const http = require('http');
 const fs = require('fs');
+const requests= require("requests");
 
 const homeFile = fs.readFileSync("home.html","utf-8");
 
-// const server
+const server = http.createServer((req,res)=>{
+    if(req.url == "/"){
+        requests(
+            "https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=5d15d09cacaa8f2ffa788844e90bea81"
+        )
+        .on("data", (chunk) =>{
+            const objdata = JSON.parse(chunk);
+            const arrData = [objdata];
+            console.log(arrData);
+        })
+        .on("end",(err)=>{
+            if (err) 
+            return console.log("connection closed due to errors",err);
+
+            console.log("end");
+        })
+    }
+});
+
+server.listen(8000, "127.0.0.1");
