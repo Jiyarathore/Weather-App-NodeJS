123034c13bf1633a7bdeebe4369bf67beb0224d4
Jiyarathore <jiyar3472@gmail.com>
2022-08-26 20:42:45 +0530

    file system

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 //5d15d09cacaa8f2ffa788844e90bea81
 //https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=5d15d09cacaa8f2ffa788844e90bea81
+
+const http = require('http');
+const fs = require('fs');
+
+const homeFile = fs.readFileSync("home.html","utf-8");
+
+// const server
